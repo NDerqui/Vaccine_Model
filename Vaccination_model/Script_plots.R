e7fa695bb1b64ac539c5fc2300ae3e4c229ea3d4
Nieves Derqui <n.derqui-fernandez@imperial.ac.uk>
2023-10-19 12:36:17 +0100

    Modified script to get results from the sum of the fit

--- Vaccination_model/Script_plots.R
+++ Vaccination_model/Script_plots.R
@@ -6,9 +6,6 @@
 #MRes Biomedical Research - EECID stream - Project 1#
 
 
-### Beter to not save the RData when closing the session ###
-
-
 
 # SET UP ------------------------------------------------------------------
 
@@ -99,53 +96,46 @@ Steps <- c(max(c(min(data_rt$date), min(data_var$date))),
 
 #### Load data ####
 
-model_name <- "CorrectVar_2000_8_3D"
+model_name <- "CorrectVar_10k_20_sum_1A"
 
-# fit <- readRDS(paste0(model_name, ".Rds"))
-  
-# loo_run <- readRDS(paste0("loo_", model_name, ".Rds"))
-
-fit <- readRDS(paste0("C:/Users/nd1316/OneDrive - Imperial College London/MRes/PROJECT 1/Analyses/Models_BackUp/", model_name, ".Rds"))
+list_result <- readRDS(paste0("C:/Users/nd1316/OneDrive - Imperial College London/MRes/PROJECT 1/Analyses/Models_BackUp/", model_name, ".Rds"))
 
 
 #### Substract parameters ####
 
-model_matrix <- as.matrix(fit)
-# rm(fit)
-# gc()
+# Parameters
 
-Rt_LogP <- colMeans(model_matrix[, grep("LogPredictions", colnames(model_matrix))])
-# Rt_LogP <- sum[grep("LogPredictions", rownames(sum)), 1]
-
-RegionalTrends <- colMeans(model_matrix[, grep("RegionalTrends", colnames(model_matrix))])
-# RegionalTrends <- sum[grep("RegionalTrends", rownames(sum)), 1]
-
-NationalTrend <- colMeans(model_matrix[, grep('^NationalTrend\\[', colnames(model_matrix))])
-# NationalTrend <- sum[grep('^NationalTrend\\[', rownames(sum)), 1]
-
-Lambda <- colMeans(model_matrix[, grep('^lambda\\[', colnames(model_matrix))])
-# Lambda <- sum[grep('^lambda\\[', rownames(sum)), 1]
+VaxEffect_data <- list_result[[2]]
+VaxEffect <- list_result[[2]][1]
 
 if (DoVariants == 1) {
-  VarAdvantage <- colMeans(model_matrix[, grep('^VarAdvantage\\[', colnames(model_matrix))])
-  # VarAdvantage <- sum[grep('^VarAdvantage\\[', rownames(sum)), 1]
+  VarAdvantage_data <- list_result[[3]]
+  VarAdvantage <- list_result[[3]][1]
+} else {
+  VarAdvantage_data <- data.frame(Null = NA)
 }
 
-sum_rt <- data.frame(LTLA, date, 
-                     Rt_data, Rt_LogP, RegionalTrends, NationalTrend,
+Rt_Predictions_data <- list_result[[4]]
+Rt_Predictions <- list_result[[4]][1]
+
+RegionalTrends_data <- list_result[[5]]
+RegionalTrends <- list_result[[5]][1]
+
+NationalTrend_data <- list_result[[6]]
+NationalTrend <- list_result[[6]][1]
+
+Lambda_data <- list_result[[7]]
+Lambda <- list_result[[7]][1]
+
+sum_rt <- data.frame(LTLA, date, Rt_data,
+                     Rt_Predictions_data, RegionalTrends_data, NationalTrend_data,
                      Dose_1, Dose_2, Dose_3)
-
-if (DoKnots == 1) {
-  sum_line <- data.frame(date = rep(Steps, times = length(unique(LTLAs))),
-                         Lambda = Lambda)
-}
 
 
 
 # DIRECTORY ---------------------------------------------------------------
 
  dir.create(here(paste0("Figures/", model_name)),recursive = TRUE)
- dir.create(here(paste0("Results/", model_name)),recursive = TRUE) 
 
 
 
@@ -154,49 +144,43 @@ if (DoKnots == 1) {
  
 #### VaxEffect ####
  
-VERedMean <- colMeans(model_matrix[, grep("VaxEffect", colnames(model_matrix))])
-# VERedMean <- sum[grep('^VaxEffect\\[', rownames(sum)), 1]
-
-VERedQuan <- colQuantiles(model_matrix[, grep("VaxEffect", colnames(model_matrix))], probs=c(0.025,0.975))
-# VERedQuan <- sum[grep('^VaxEffect\\[', rownames(sum)), c(4,8)]
+# Change rownames for easier interpretation
  
-sum_ve <- round(data.frame(VERedMean, VERedQuan), digits = 4) %>%
-  select("VERedMean", "X2.5.", "X97.5.")
-colnames(sum_ve) <- c("VE (1-Red)", " VE 2.5% Q", " VE97.5% Q")
-
-if (nrow(sum_ve) == 3) {
-  row.names(sum_ve) <- c("Dose 1", "Dose 2", "Dose 3")
+if (nrow(VaxEffect_data) == 3) {
+  row.names(VaxEffect_data) <- c("Dose 1", "Dose 2", "Dose 3")
 
 } else {
   
-  if(nrow(sum_ve) == 6) {
-    row.names(sum_ve) <- c("Alpha1", "Alpha2", "Alpha3",
-                           "Delta1", "Delta2", "Delta3")
+  if(nrow(VaxEffect_data) == 6) {
+    row.names(VaxEffect_data) <- c("Alpha1", "Alpha2", "Alpha3",
+                                   "Delta1", "Delta2", "Delta3")
     } else {
       
       if (DoAge == 1) {
-        row.names(sum_ve) <- c("15-49_D1", "15-49_D2", "15-49_D3",
-                               "50-69_D1", "50-69_D2", "50-69_D3",
-                               "70+_D1", "70+_D2", "70+_D3")
+        row.names(VaxEffect_data) <- c("15-49_D1", "15-49_D2", "15-49_D3",
+                                       "50-69_D1", "50-69_D2", "50-69_D3",
+                                       "70+_D1", "70+_D2", "70+_D3")
       } else {
-        row.names(sum_ve) <- c("PreAl_1", "PreAl_2", "PreAl_3",
-                               "Alpha1", "Alpha2", "Alpha3",
-                               "Delta1", "Delta2", "Delta3")
+        row.names(VaxEffect_data) <- c("PreAl_1", "PreAl_2", "PreAl_3",
+                                       "Alpha1", "Alpha2", "Alpha3",
+                                       "Delta1", "Delta2", "Delta3")
       }
   }
 }
                 
-write.xlsx(sum_ve, rowNames = TRUE,
-            paste0("Results/", model_name, "/Vax_VE_table.xlsx"))
 
+#### Save ####
+ 
+table_list <- list("VaxEffect" = VaxEffect_data,
+                   "VarAdvantage" = VarAdvantage_data,
+                   "RtPredictions" = Rt_Predictions_data,
+                   "RegionalTrends" = RegionalTrends_data,
+                   "NationalTrends" = NationalTrend_data,
+                   "Lambda" = Lambda_data)
+ 
+write.xlsx(table_list, rowNames = TRUE,
+           file = paste0("Results/", model_name, "_results_table.xlsx"))
 
-#### Var Advantage ####
-
-if (DoVariants == 1) {
-  
-  write.xlsx(as.data.frame(VarAdvantage), rowNames = TRUE,
-             paste0("Results/", model_name, "/Var_Ad_table.xlsx"))
-}
 
  
  
@@ -211,7 +195,7 @@ png(paste0("Figures/", model_name, "/Obs_Pre_Rt.png"),
     width = 9, height = 8, units = 'in', res = 600)
  
 ggplot(data = sum_rt) +
-   geom_point(mapping = aes(x = Rt_data, y = Rt_LogP), size = rel(0.8)) +
+   geom_point(mapping = aes(x = Rt_data, y = Rt), size = rel(0.8)) +
    geom_abline(x = 0, y = 1, col = "red") +
    theme_classic() +
    labs(title = "Observed versus Predicted Rt in all LTLAs and timepoints",
@@ -233,7 +217,7 @@ png(paste0("Figures/", model_name, "/Pre_Obs_Rt_time.png"),
 ggplot(data = sum_rt) +
   geom_boxplot (mapping = aes(x = date, y = Rt_data, group = date,
                                color = "Rt_data"), size = rel(0.5)) +
-  geom_boxplot (mapping = aes(x = date, y = Rt_LogP, group = date,
+  geom_boxplot (mapping = aes(x = date, y = Rt, group = date,
                                color = "Rt_LogP"), size = rel(0.5)) + 
   scale_color_manual(name="Reproduction number",
                       breaks = c("Rt_data", "Rt_LogP"),
@@ -256,57 +240,8 @@ ggplot(data = sum_rt) +
 
 dev.off()
 
- 
-#### Reg Trend ####
- 
-png(paste0("Figures/", model_name, "/RegionalTrend.png"),
-    width = 10, height = 6, units = 'in', res = 600)
- 
-ggplot(data = sum_rt) +
-  geom_boxplot (mapping = aes(x = date, y = RegionalTrends, group = date), 
-                 size = rel(0.5)) +
-  theme_classic() +
-   labs(title = "Regional Trends in all LTLAs over time",
-        x = "Date",
-        y = "Secular Trend") +
-  theme(
-     plot.title = element_text(size = rel(1), face="bold", hjust = 0.5),
-     axis.title.x = element_text(size = rel(0.9), face="bold"),
-     axis.title.y = element_text(size = rel(0.9), face="bold"),
-     axis.text = element_text(size=rel(0.7)),
-     legend.title = element_text(size = rel(0.9), face="bold"),
-     legend.text = element_text(size=rel(0.7)))+
-  if (DoKnots == 1) {
-    geom_vline(xintercept = as.Date(Steps, format = "%d/%m/%Y"),
-               color = "darkmagenta")}
 
-dev.off()
- 
- 
 #### National Trend ####
- 
-png(paste0("Figures/", model_name, "/NationalTrend.png"),
-    width = 10, height = 6, units = 'in', res = 600)
- 
-ggplot(data = sum_rt) +
-  geom_boxplot (mapping = aes(x = date, y = NationalTrend, group = date), 
-                 size = rel(0.5)) +
-  theme_classic() +
-  labs(title = "National Trend over time",
-        x = "Date",
-        y = "National Trend") +
-  theme(
-     plot.title = element_text(size = rel(1), face="bold", hjust = 0.5),
-     axis.title.x = element_text(size = rel(0.9), face="bold"),
-     axis.title.y = element_text(size = rel(0.9), face="bold"),
-     axis.text = element_text(size=rel(0.7)),
-     legend.title = element_text(size = rel(0.9), face="bold"),
-     legend.text = element_text(size=rel(0.7)))+
-  if (DoKnots == 1) {
-    geom_vline(xintercept = as.Date(Steps, format = "%d/%m/%Y"),
-               color = "darkmagenta")}
-
-dev.off()
  
 # Spline: National Trend plotted with knots 
 
@@ -356,7 +291,7 @@ ggplot(data = some_LTLA) +
                            color = "Lambda"), linewidth = 1.3) +
   geom_line (mapping = aes(x = date, y = RegionalTrends,
                            color = "RanEffect"), linewidth = 1.3) +
-  geom_line (mapping = aes(x = date, y = Rt_LogP,
+  geom_line (mapping = aes(x = date, y = Rt,
                            color = "RtLogP"), linewidth = 1.3) +
   geom_line (mapping = aes(x = date, y = Rt_data,
                            color = "RtData"), linewidth = 1.3) +
@@ -381,6 +316,58 @@ ggplot(data = some_LTLA) +
      legend.title = element_text(size = rel(1.3), face="bold"),
      legend.position = "bottom",
      legend.text = element_text(size=rel(1.2))) +
+  facet_wrap(LTLA ~ .)
+
+dev.off()
+
+png(paste0("Figures/", model_name, "/LTLA_plot.png"),
+    width = 12, height = 8, units = 'in', res = 300)
+
+ggplot(data = some_LTLA) +
+  geom_line (mapping = aes(x = date, y = NationalTrend, 
+                           color = "Lambda")) +
+  geom_ribbon(mapping = aes(x = date, ymin = X2.5..2, ymax = X97.5..2, 
+                           fill = "Lambda"), alpha = 0.2) +
+  geom_line (mapping = aes(x = date, y = RegionalTrends,
+                           color = "RanEffect")) +
+  geom_ribbon(mapping = aes(x = date, ymin = X2.5..1, ymax = X97.5..1,
+                           fill = "RanEffect"), alpha = 0.2) +
+  geom_line (mapping = aes(x = date, y = Rt,
+                           color = "RtLogP")) +
+  geom_ribbon(mapping = aes(x = date, ymin = X2.5., ymax = X97.5.,
+                           fill = "RtLogP"), alpha = 0.2) +
+  geom_line (mapping = aes(x = date, y = Rt_data,
+                           color = "RtData")) +
+  scale_color_manual(name = "Parameter",
+                     breaks = c("Lambda", "RanEffect",
+                                "RtLogP", "RtData"),
+                     values = c("Lambda" = "navy",
+                                "RanEffect" = "lightgreen",
+                                "RtLogP" = "forestgreen",
+                                "RtData" = "firebrick"),
+                     labels = c("National Trend", "Regional Trend",
+                                "Predicted Rt", "Observed Rt")) +
+  scale_fill_manual(name = "Parameter",
+                     breaks = c("Lambda", "RanEffect",
+                                "RtLogP", "RtData"),
+                     values = c("Lambda" = "navy",
+                                "RanEffect" = "lightgreen",
+                                "RtLogP" = "forestgreen",
+                                "RtData" = "firebrick"),
+                     labels = c("National Trend", "Regional Trend",
+                                "Predicted Rt", "Observed Rt")) +
+  theme_classic() +
+  labs(title = "Parameters in various LTLAs",
+       x = "Date",
+       y = "Parameter value") +
+  theme(
+    plot.title = element_text(size = rel(1.5), face="bold", hjust = 0.5),
+    axis.title.x = element_text(size = rel(1.3), face="bold"),
+    axis.title.y = element_text(size = rel(1.3), face="bold"),
+    axis.text = element_text(size=rel(1.2)),
+    legend.title = element_text(size = rel(1.3), face="bold"),
+    legend.position = "bottom",
+    legend.text = element_text(size=rel(1.2))) +
   facet_wrap(LTLA ~ .)
 
 dev.off()
